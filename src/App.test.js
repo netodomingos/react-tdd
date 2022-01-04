51de153707ec8ef2cf8861e849c2b5ef319c34d0
Geraldo-Thorpe <64435314+geraldo-thorpe-innovation@users.noreply.github.com>
2022-01-03 23:46:11 -0300

    organizacao testes

--- src/App.test.js
+++ src/App.test.js
@@ -1,9 +1,6 @@
-import { render, screen } from '@testing-library/react';
 import App from './App';
-
 import { configure, shallow } from 'enzyme';
 import Adapter from 'enzyme-adapter-react-16';
-import { text } from 'express';
 
 configure({ adapter: new Adapter() });
 
@@ -11,16 +8,18 @@ describe('counter testing', () => {
   let wrapper;
 
   beforeEach(() => { wrapper = shallow(<App />) })
+  
+  // Application State & Structure
   test('render the title of counter', () => {
     expect(wrapper.find("h1").text()).toContain("this is counter app")
   });
+  test('render the value of state in a div', () => {
+    expect(wrapper.find("#counter-value").text()).toBe("0")
+  })
 
   // Increment Block
   test('render a button with text of `increment`', () => {
     expect(wrapper.find("#increment-btn").text()).toBe("Increment")
-  })
-  test('render the incremental value of state in a div', () => {
-    expect(wrapper.find("#counter-value").text()).toBe("0")
   })
   test('render click event of INCREMENT button and INCREMENT counter value', () => {
     wrapper.find("#increment-btn").simulate("click")
